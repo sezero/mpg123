741bda4e995604ac7cd5eeef330a15f6431d380c
Ozkan Sezer <sezeroz@gmail.com>
2021-07-07 11:55:50 +0300

    backport fix of part2_3_length regression (bug 312)
    from 1.25.15.

--- src/layer3.c
+++ src/layer3.c
@@ -548,6 +548,9 @@ static int III_get_side_info(mpg123_handle *fr, struct III_sideinfo *si,int ster
 	return 0;
 }
 
+/* Even with part2_3_length == 0, the few bit reads here are no danger.
+   They are only of a limited amount and the buffer is big. Later versions
+   of mpg123 check their number in advance for consistency. */
 
 /* read scalefactors */
 static int III_get_scale_factors_1(mpg123_handle *fr, int *scf,struct gr_info_s *gr_info,int ch,int gr)
@@ -560,14 +563,6 @@ static int III_get_scale_factors_1(mpg123_handle *fr, int *scf,struct gr_info_s 
 	int numbits;
 	int num0 = slen[0][gr_info->scalefac_compress];
 	int num1 = slen[1][gr_info->scalefac_compress];
-
-	if(gr_info->part2_3_length == 0)
-	{
-		int i;
-		for(i=0;i<39;i++)
-			*scf++ = 0;
-		return 0;
-	}
 
 	if(gr_info->block_type == 2)
 	{
@@ -682,14 +677,6 @@ static int III_get_scale_factors_2(mpg123_handle *fr, int *scf,struct gr_info_s 
 	}
 
 	pnt = stab[n][(slen>>12)&0x7];
-
-	if(gr_info->part2_3_length == 0)
-	{
-		int i;
-		for(i=0;i<39;i++)
-			*scf++ = 0;
-		return 0;
-	}
 
 	for(i=0;i<4;i++)
 	{
